57ab7c214145c13f6a1685bbe7e64d3d312500dd
Overtorment <overtorment@gmail.com>
2021-10-26 12:11:47 +0100

    publish build

--- _next/static/jXkHq7vus0f9rndC80lL4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/246-95e3632316189152b0be.js","static/css/81350cacca8d21e80c3b.css","static/chunks/pages/index-673e80eea1240d720f27.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
